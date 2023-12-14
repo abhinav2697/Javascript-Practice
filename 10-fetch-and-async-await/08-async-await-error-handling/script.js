8ca6f7a9e779b11c27ed1d6ee1aeee14f6fe99e4
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2023-12-14 15:32:18 +0530

    Update script.js

---
+++ 10-fetch-and-async-await/08-async-await-error-handling/script.js
@@ -0,0 +1,55 @@
+// const getUsers = async () => {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// };
+
+
+// const getUsers = async () => {
+//     try {
+//         // const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const response = await fetch('https://httpstat.us/404');
+//         const data = await response.text();
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// };
+
+
+// const getPosts = async () => {
+//     try {
+//          const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         // const response = await fetch('https://httpstat.us/404');
+
+//         if (!response.ok) {
+//             throw new Error('Request Failed');
+//         }
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+// };
+
+
+const getPosts = async () => {
+    const response = await fetch('https://httpstat.us/500');
+    if (!response.ok) {
+        throw new Error('Request Failed');
+    }
+    const data = await response.text();
+    console.log(data);
+
+}
+
+
+//getUsers();
+getPosts().catch((error)=>console.log(error));
