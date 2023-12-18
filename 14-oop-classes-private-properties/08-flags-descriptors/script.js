b2a9b32c3749d09cfe383a958e5ea168936d454c
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2023-12-18 16:21:41 +0530

    Update script.js

--- 14-oop-classes-private-properties/08-flags-descriptors/script.js
+++ 14-oop-classes-private-properties/08-flags-descriptors/script.js
@@ -2,3 +2,40 @@
 // [[Enumerable]] - if `true`, the property will be returned in a `for...in` loop, otherwise not
 // [[Writable]] - if `true`, the value of the property can be changed, otherwise not
 // [[Value]] - the value of the property
+Math.PI = 4;
+console.log(Math.PI);
+
+let descriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
+const rectObj = {
+    name: 'Rectangle',
+    width: 10,
+    height: 10,
+
+};
+
+
+Object.defineProperty(rectObj, 'name', {
+    writable: false,
+    configurable: false,
+    enumerable: false,
+    descriptor: Object.getOwnPropertyDescriptor(rectObj, 'name')
+});
+descriptor = Object.getOwnPropertyDescriptor(rectObj, 'name');
+console.log(descriptor);
+
+    
+    
+// descriptor = Object.getOwnPropertyDescriptor(rectObj, 'name');
+// console.log(descriptor);
+
+
+rectObj.name = 'New Name';
+delete rectObj.name;
+//delete rectObj.width;
+console.log(rectObj);
+
+for (let [key, value] of Object.entries(rectObj)) {
+    console.log(`${key}:${value}`);
+}
+
+console.log(Object.getOwnPropertyDescriptor(rectObj));
