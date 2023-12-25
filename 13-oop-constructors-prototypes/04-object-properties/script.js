0c0fd862c647d1770f15590b83cd1e2606c45e75
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2023-12-25 22:38:54 +0530

    Update script.js

--- 13-oop-constructors-prototypes/04-object-properties/script.js
+++ 13-oop-constructors-prototypes/04-object-properties/script.js
@@ -6,3 +6,35 @@ function Rectangle(name, width, height) {
     return this.width * this.height;
   };
 }
+
+const rect1 = new Rectangle('Rectangle 1', 20, 20);
+const rect2 = new Rectangle('Rectangle 2', 20, 30);
+console.log(rect1.name, rect2.width);
+console.log(rect1['width']);
+rect1.color = 'red';
+rect2.perimeter = () => 2 * (rect2.width + rect2.height);
+console.log(rect2.perimeter());
+console.log(rect1);
+//delete property
+delete rect2.perimeter;
+
+
+//check for property
+console.log(rect2.hasOwnProperty('color'));
+console.log(rect1.hasOwnProperty('color'));
+
+//Get keys
+console.log(Object.keys(rect1));
+
+//get values
+console.log(Object.values(rect2));
+
+//Get entries
+console.log(Object.entries(rect1));
+
+for (let [key, values] of Object.entries(rect1)) {
+  if (typeof value !== 'function') {
+    console.log(`${key}-${value}`);
+  }
+}
+console.log(rect2);
